2b66366d9d999ca79d1fe2b808e53651cdf2fbfc
buddha314 <buddha_314@yahoo.com>
2018-03-20 17:29:04 -0700

    adding method to print simple grids, could still use some work but gets the idea across

--- macbuddha.local.mk
+++ macbuddha.local.mk
@@ -1,6 +1,6 @@
 CHINGON_HOME=/Users/buddha/github/buddha314/chingon
 NUMSUCH_HOME=/Users/buddha/github/buddha314/numsuch
-CDO_HOME=/Users/buddha/github/buddha314/cdo
+CDO_HOME=/Users/buddha/github/cdo
 CDOEXTRAS_HOME=/Users/buddha/github/buddha314/CdoExtras
 BLAS_HOME=/usr/local/Cellar/openblas/0.2.20
 POSTGRES_HOME=/usr/include/postgresql
